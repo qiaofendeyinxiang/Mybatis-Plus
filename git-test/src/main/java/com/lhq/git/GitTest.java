97f3eabf1a83cf44f2b9aae0784b5ad8f0fad80a
jiumozhi <jiumozhi@qq.com>
2022-09-08 10:02:15 +0800

    second8 commit

--- git-test/src/main/java/com/lhq/git/GitTest.java
+++ git-test/src/main/java/com/lhq/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello git5 hot-fix");
         System.out.println("master git");
         System.out.println("hot-fix git");
+        System.out.println("git push");
     }
 }
